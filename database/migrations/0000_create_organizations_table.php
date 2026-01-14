61761c858c2f9063b424b9a829939e46482b9fa5
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-01-14 07:26:23 +0700

    feat: add name and type columns to organizations

--- database/migrations/0000_create_organizations_table.php
+++ database/migrations/0000_create_organizations_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type');
             $table->timestamps();
         });
     }
