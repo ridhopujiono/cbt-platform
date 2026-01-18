a94044e6841cdbaee76af42037617d6f582f0dfb
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-01-18 17:08:46 +0700

    feat: Enable Telescope for local development

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local')) {
+            if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+                $this->app->register(
+                    \App\Providers\TelescopeServiceProvider::class
+                );
+            }
+        }
     }
 
     /**
